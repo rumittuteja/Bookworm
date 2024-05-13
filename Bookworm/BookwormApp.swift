d73e36b8a86ef9782a52d760c863fc011567c2fa
Rumit Singh Tuteja <rtuteja@i-360.com>
2024-05-13 15:52:03 -0400

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Rumit Singh Tuteja on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
